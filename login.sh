18d4cdf2f34dd9b4c3ff76131cb026613eadd003
ajikamaludin <aji19kamaludin@gmail.com>
2022-09-07 22:01:41 +0700

    update in

--- login.sh
+++ login.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
-curl --location --request POST 'https://api-rayatech.morhuman.id/api/mobile/v4/login/login' \
---header 'api-key: MOR-KEY-yvX*pbVgeunZsSa4J7feF' \
---header 'Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyYW5kMSI6MTE0OTkwNTc2NywicmFuZDIiOjIwNDQwODA3NzV9.0Cv-LAQE5X_NO21EO5kALs2EmiEskCpp1QPRJ4dSbqo' \
---header 'Cookie: ci_session=rqmp4n5l887iugatcdd86oq3ajt9fi91' \
---form 'username="aji.ka"' \
---form 'password="Password123."'
+curl https://api-kasiraja.ajikamaludin.id
